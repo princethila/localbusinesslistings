1531d6479f9d16dee7400abb209dbbcd042d9c60
princethila <33749524+princethila@users.noreply.github.com>
2024-11-04 19:05:50 +0200

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,17 @@
-import DeployButton from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import HeaderAuth from "@/components/header-auth";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-import { GeistSans } from "geist/font/sans";
-import { ThemeProvider } from "next-themes";
-import Link from "next/link";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Link from "@/node_modules/next/link";
+import {
+  BuildingStorefrontIcon
+} from '@heroicons/react/24/outline';
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
-};
+export const metadata: Metadata = {
+  title: 'Trending on Instagram',
+  description: 'See who is trending and how',
+}
 
 export default function RootLayout({
   children,
@@ -24,48 +19,31 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className} suppressHydrationWarning>
-      <body className="bg-background text-foreground">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
-                  </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
-                </div>
-              </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-                {children}
-              </div>
-
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
+    <html lang="en">
+      <body className={inter.className}>
+        <nav className='top-0 sticky bg-white/30 backdrop-blur-md px-6 sm:px-24 z-10'>
+          <div className="pt-12 pb-12">
+            <div className="mx-auto px-2 border-x-0">
+              <div className="flex justify-between items-center h-16">
+                <div>
+                  <Link
+                  href ={'/'}
                   >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
-              </footer>
+                    <div className='flex items-center'>
+                      <BuildingStorefrontIcon className="mr-1.5 h-7 w-7 " aria-hidden="true" />
+                      <h1 className="text-lg font-bold font-sans pr-2 ">list-a-local-business</h1>
+                    </div>
+                    <div>
+                      <p className='py-2 text-gray-500 text-xs line-clamp-1'>Discover and support local businesses</p>
+                    </div>
+                  </Link>
+                </div>
+                
+              </div>
             </div>
-          </main>
-        </ThemeProvider>
+          </div>
+        </nav>
+          {children}
       </body>
     </html>
   );
