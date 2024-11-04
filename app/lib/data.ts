17727c126700f7e1a3048b9c26f62198519162fe
princethila <33749524+princethila@users.noreply.github.com>
2024-11-04 19:18:27 +0200

    added data and definitions

---
+++ app/lib/data.ts
@@ -0,0 +1,14 @@
+"use server";
+import { createClient } from "@/utils/supabase/server";
+import { Categories, Businesses, Business} from "./definitions";
+const supabase = createClient();
+
+export async function getCategories(): Promise<Categories[]>{
+    try{
+        const { data: categories } = await supabase.from("categories").select();
+        return categories as Categories[]
+    }catch (error) {
+        console.error('Database Error:', error);
+        throw new Error('Failed to fetch categories.');
+      }
+}
---
+++ app/lib/definitions.ts
@@ -0,0 +1,37 @@
+export type Categories = {
+    id: string,
+    value: string,
+    label: string,
+}
+
+
+export type Businesses = {
+    id: string,
+    business_name: string,
+    description: string,
+    area: string,
+    categories: string,
+    phone_number: string,
+    email_address: string,
+    telegram_username: string,
+    whatsapp_number: string,
+    posted_by: string,
+    verified: boolean,
+    products_services: string
+}
+
+
+export type Business = {
+    id: string,
+    business_name: string,
+    description: string,
+    area: string,
+    categories: string,
+    phone_number: string,
+    email_address: string,
+    telegram_username: string,
+    whatsapp_number: string,
+    posted_by: string,
+    verified: boolean,
+    products_services: string
+}
